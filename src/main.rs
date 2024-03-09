58a3d7fabce02b192bedd5e76ee431478c35dfc8
Itta Funahashi <itta@itta.dev>
2024-03-09 15:47:24 +0900

    feat: implement stat structure

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
-// use std::net::TcpListener;
+use reqwest;
 use tokio::time::{self, Duration};
+
+struct Stat {
+    tempreture: i32,
+    memory: i32,
+    cpu: f32,
+}
 
 #[tokio::main]
 async fn main() {
-    // TcpListener::bind("127.0.0.1:2784").unwrap();
     let mut interval = time::interval(Duration::from_secs(10));
+    let client = reqwest::Client::new();
 
     loop {
-        interval.tick().await;
+        let stat = Stat {
+            tempreture: 40,
+            memory: 4,
+            cpu: 0.2,
+        };
+        client.post("https://192.168.1.101:2784").body(stat);
         println!("Hello, world!");
+        interval.tick().await;
     }
 }

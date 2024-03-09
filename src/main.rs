39b7a9e18ef265140f71a21aa6a89f01fdffd601
Itta Funahashi <itta@itta.dev>
2024-03-09 14:49:55 +0900

    feat: install tokio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+// use std::net::TcpListener;
+use tokio::time::{self, Duration};
+
+#[tokio::main]
+async fn main() {
+    // TcpListener::bind("127.0.0.1:2784").unwrap();
+    let mut interval = time::interval(Duration::from_secs(10));
+
+    loop {
+        interval.tick().await;
+        println!("Hello, world!");
+    }
 }

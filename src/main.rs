7cd71dffb1e45a558327c5bee70311f45f411a54
Itta Funahashi <itta@itta.dev>
2024-03-10 11:58:06 +0900

    feat: error handling

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,26 @@ async fn main() {
     let mut interval = time::interval(Duration::from_secs(10));
     let client = reqwest::Client::new();
     let mut stats = Stats::new();
+    let master_ip = "192.168.1.101";
+    let master_port = "2784";
 
     loop {
         stats.update();
-        client
-            .post("https://192.168.1.101:2784")
-            .body(stats.to_json())
-            .send()
-            .await
-            .unwrap();
-        println!("Hello, world!");
+
+        let url = format!("http://{}:{}", master_ip, master_port);
+        let result = client.post(url).body(stats.to_json()).send().await;
+
+        if result.is_err() {
+            panic!(
+                "
+Failed to connect to master node ({}):
+Check host server is running correctly.
+
+",
+                master_ip
+            );
+        }
+
         interval.tick().await;
     }
 }
